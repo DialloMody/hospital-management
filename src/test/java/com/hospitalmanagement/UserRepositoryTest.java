70baf592f1d93a0f8c99edf5985e35688cba865e
Diallo Mody <mody.diallo.12@gmail.com>
2025-10-22 22:27:05 +0200

    Addition d'une classe test pour l'utilisateur

--- src/test/java/com/hospitalmanagement/UserRepositoryTest.java
+++ src/test/java/com/hospitalmanagement/UserRepositoryTest.java
@@ -1,4 +1,34 @@
 package com.hospitalmanagement;
 
-public class UserRepositoryTest {
+import com.hospitalmanagement.model.Role;
+import com.hospitalmanagement.model.User;
+import com.hospitalmanagement.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Set;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class UserRepositoryTest
+{
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void saveUserAndRole()
+    {
+        User user = User.builder()
+                .username("admin")
+                .email("admin@hopital.com")
+                .password("1234")
+                .roles(Set.of(Role.ADMIN))
+                .build();
+
+        User savedUser = userRepository.save(user);
+
+        assertThat(savedUser.getId()).isNotNull();
+        assertThat(savedUser.getRoles()).contains(Role.ADMIN);
+    }
 }
