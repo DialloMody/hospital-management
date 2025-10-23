2c964022b605443531989459c91d866fa059171a
Diallo Mody <mody.diallo.12@gmail.com>
2025-10-23 18:08:51 +0200

    Service d'authentication

---
+++ src/main/java/com/hospitalmanagement/service/AuthService.java
@@ -0,0 +1,51 @@
+package com.hospitalmanagement.service;
+
+import com.hospitalmanagement.model.User;
+import com.hospitalmanagement.repository.UserRepository;
+import com.hospitalmanagement.security.dto.AuthResponse;
+import com.hospitalmanagement.security.dto.LoginRequest;
+import com.hospitalmanagement.security.dto.RegisterRequest;
+import com.hospitalmanagement.security.jwt.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService
+{
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthResponse register(RegisterRequest request)
+    {
+        User user = User.builder()
+                .username(request.getUsername())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .roles(request.getRoles())
+                .build();
+
+        userRepository.save(user);
+
+        String token = jwtService.generateToken(user.getUsername());
+        return new AuthResponse(token);
+    }
+
+    public  AuthResponse login(LoginRequest request)
+    {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+        );
+
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new RuntimeException("Utilisateur pas trouvé"));
+
+        String token = jwtService.generateToken(user.getUsername());
+        return new AuthResponse(token);
+    }
+}
